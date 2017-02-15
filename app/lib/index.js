981ea0d956de63adf3b3106dd7bfe70febf21f4b
ivelkov <ivelkov@axway.com>
2017-02-15 11:38:44 +0200

    Do not use asar since this doesn't work with open which leverages the exec call

--- app/lib/index.js
+++ app/lib/index.js
@@ -7,6 +7,7 @@ const open = require('open');
 
 const Tray = electron.Tray;
 const Menu = electron.Menu;
+let shouldQuit = false;
 
 const app = electron.app;
 app.on('ready', () => {
@@ -32,30 +33,43 @@ app.on('ready', () => {
       window.focus();
     }
   });
-  tray.setContextMenu(new Menu.buildFromTemplate([
-    {
-      label: 'Open',
-      click: () => {
-        window.show();
+  tray.setContextMenu(new Menu.buildFromTemplate(
+    [
+      {
+        label: 'Refresh',
+        click: () => {
+          window.show();
+          window.reload();
+        }
+      },
+      {
+        label: 'Quit',
+        click: () => {
+          shouldQuit = true;
+          app.quit();
+        }
       }
-    },
-    {
-      label: 'Refresh',
-      click: () => {
-        window.show();
-        window.reload();
-      }
-    },
-    {
-      label: 'Quit',
-      click: app.quit
+    ]
+  ));
+
+  window.on('close',  (event) => {
+    if (!shouldQuit) {
+      event.preventDefault();
+      window.hide();
+    } else {
+      app.quit();
     }
-  ]));
+  });
 
   window.webContents.on('new-window', (event, url) => {
     event.preventDefault();
-    open(url);
+    open(url, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${err}`);
+      }
+    });
   });
+
   window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
   window.loadURL('https://teams.microsoft.com/');
 });
