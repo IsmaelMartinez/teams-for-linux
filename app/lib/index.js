07b09ab2738de491eac37d7eb01691d462898914
ivelkov <ivelkov@axway.com>
2017-02-15 10:20:32 +0200

    Adding tray icon; Open links in external browser

--- app/lib/index.js
+++ app/lib/index.js
@@ -1,18 +1,60 @@
 
+'use strict';
+
 const electron = require('electron');
 const path = require('path');
+const open = require('open');
+
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 
 const app = electron.app;
 app.on('ready', () => {
+  const icon = path.join(app.getAppPath(), 'lib/assets/icons/icon-96x96.png');
   const window = new electron.BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(app.getAppPath(), 'lib/assets/icons/icon-96x96.png'),
+    icon,
 
     webPreferences: {
       partition: 'persist:teams',
       nodeIntegration: false
     }
+  });
+
+  const tray = new Tray(icon);
+  tray.setToolTip('Teams');
+  tray.on('click', () => {
+    if (window.isFocused()) {
+      window.hide();
+    } else {
+      window.show();
+      window.focus();
+    }
+  });
+  tray.setContextMenu(new Menu.buildFromTemplate([
+    {
+      label: 'Open',
+      click: () => {
+        window.show();
+      }
+    },
+    {
+      label: 'Refresh',
+      click: () => {
+        window.show();
+        window.reload();
+      }
+    },
+    {
+      label: 'Quit',
+      click: app.quit
+    }
+  ]));
+
+  window.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    open(url);
   });
   window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
   window.loadURL('https://teams.microsoft.com/');
