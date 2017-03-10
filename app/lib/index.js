5233d53dc39e3021736f0927bc33f8655ba1a4a7
Daniel Matthies <mageluingil@gmail.com>
2017-03-10 08:33:00 -0700

    Added window menu

--- app/lib/index.js
+++ app/lib/index.js
@@ -4,7 +4,7 @@
 const electron = require('electron');
 const path = require('path');
 const open = require('open');
-const nativeImage = require('electron').nativeImage;
+const NativeImage = electron.nativeImage;
 
 const Tray = electron.Tray;
 const Menu = electron.Menu;
@@ -13,11 +13,12 @@ let tray;
 
 const app = electron.app;
 app.on('ready', () => {
-  const initialIcon = path.join(app.getAppPath(), 'lib/assets/icons/icon-96x96.png');
+  const icon = path.join(app.getAppPath(), 'lib/assets/icons/icon-96x96.png');
   const window = new electron.BrowserWindow({
     width: 800,
     height: 600,
-    initialIcon,
+    icon: icon,
+    autoHideMenuBar: true,
 
     webPreferences: {
       partition: 'persist:teams',
@@ -26,8 +27,55 @@ app.on('ready', () => {
     }
   });
 
-  tray = new Tray(initialIcon);
-  tray.setToolTip('Teams');
+  // Build menu for window and tray
+  let app_menu = new Menu.buildFromTemplate(
+    [
+      {
+        label: 'Refresh',
+        accelerator: 'ctrl+R',
+        click: () => {
+          window.show();
+          window.reload();
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: 'ctrl+Q',
+        click: () => {
+          shouldQuit = true;
+          app.quit();
+        }
+      }
+    ]
+  );
+  window.setMenu(new Menu.buildFromTemplate([
+    {
+      label: 'File',
+      submenu: app_menu
+    },
+    {
+      label: 'Help',
+      submenu: [
+        {
+            label: 'Online Documentation',
+            click: () => open('https://support.office.com/en-us/teams?omkt=en-001')
+        },
+        {
+          label: 'Github Project',
+          click: () => open('https://github.com/ivelkov/teams-for-linux')
+        },
+        { type: 'separator' },
+        {
+          label: 'Version ' + app.getVersion(),
+          enabled: false
+        }
+      ]
+    }
+  ]));
+
+  // Create tray icon
+  tray = new Tray(icon);
+  tray.setToolTip('Microsoft Teams');
   tray.on('click', () => {
     if (window.isFocused()) {
       window.hide();
@@ -36,25 +84,7 @@ app.on('ready', () => {
       window.focus();
     }
   });
-
-  tray.setContextMenu(new Menu.buildFromTemplate(
-    [
-      {
-        label: 'Refresh',
-        click: () => {
-          window.show();
-          window.reload();
-        }
-      },
-      {
-        label: 'Quit',
-        click: () => {
-          shouldQuit = true;
-          app.quit();
-        }
-      }
-    ]
-  ));
+  tray.setContextMenu(app_menu);
 
   window.on('close', (event) => {
     if (!shouldQuit) {
@@ -68,7 +98,7 @@ app.on('ready', () => {
   window.on('page-title-updated', (event, title) => window.webContents.send('page-title', title));
   electron.ipcMain.on('notifications', (event, {count, icon}) => {
     try {
-      const image = nativeImage.createFromDataURL(icon);
+      const image = NativeImage.createFromDataURL(icon);
       tray.setImage(image);
       window.flashFrame(count > 0);
     } catch (err) {
