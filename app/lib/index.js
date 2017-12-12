406d3e2c9d9ccdf65bca84bcab599a0f135c3e02
ivelkov <ivelkov@axway.com>
2017-12-12 17:47:31 +0200

    Upgrading electron and electron-builder to latest Updating HTTP User-Agent to mock chrome 60 which is the minimum supported version by teams

--- app/lib/index.js
+++ app/lib/index.js
@@ -46,7 +46,7 @@ app.on('ready', () => {
     });
   });
 
-  window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
+  window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'); 
   window.loadURL('https://teams.microsoft.com/');
 
   if (process.env.WEB_DEBUG) {
