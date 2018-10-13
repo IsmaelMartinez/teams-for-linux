a72c3c3940281ab60a679eafca434ecfab7cdeeb
ismaelmartinez <ismaelmartinez@gmail.com>
2018-10-13 21:08:37 +0100

    Adding the zoomclick in the browser as the shorcuts from the electron role funciontality are not working

--- app/lib/browser/index.js
+++ app/lib/browser/index.js
@@ -1,12 +1,13 @@
 'use strict';
 //require('./renderer.js');
-require('electron-notification-shim');
+//require('electron-notification-shim');
 
 (function () {
   const path = require('path');
   const { ipcRenderer, webFrame } = require('electron');
   const trayNotifications = require('./tray-notifications');
   const nativeNotifications = require('./native-notifications');
+  const zoom = require('./zoom');
   
   const iconPath = path.join(__dirname, '../assets/icons/icon-96x96.png');
 
