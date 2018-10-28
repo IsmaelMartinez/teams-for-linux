23837a05ace721aa5f418d0ab47e03414b87caa9
IsmaelMartinez <ismaelmartinez@gmail.com>
2018-10-28 19:56:45 +0000

    Changing a bit the pull request #5 to only show the ': ' if there is a message and not in all the cases

--- app/lib/index.js
+++ app/lib/index.js
@@ -64,7 +64,7 @@ app.on('ready', () => {
 
   ipcMain.on('notifications', async (e, msg) => {
     if (msg.count > 0) {
-      const body = "(" + msg.count + "): " + ((msg.text) ? "" + msg.text : "");
+      const body = "(" + msg.count + ")" + ((msg.text) ? ": " + msg.text : "");
       const notification = new NativeNotification(
         "Microsoft Teams",
         {
