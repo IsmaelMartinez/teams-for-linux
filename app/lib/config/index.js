387074efff180e3fe99117d44fc52cbc03370641
Ismael <ismaelmartinez@gmail.com>
2018-10-29 11:31:47 +0000

    updating the user agent for edge to 17.17134

--- app/lib/config/index.js
+++ app/lib/config/index.js
@@ -31,7 +31,7 @@ function argv(configPath) {
         describe: 'Microsoft Edge User Agent',
         type: 'string',
         default:
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/12.246'
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
       },
       chromeUserAgent: {
         describe: 'Google Chrome User Agent',
