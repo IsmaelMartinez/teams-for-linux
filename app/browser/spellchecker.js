29c261db0372b1fc2081e5a0880cdea81eb0f1ad
Ismael Martinez <ismael.martinez@postcodelottery.co.uk>
2018-12-14 21:08:45 +0000

    Adding the basic spellcheck with locale detection (from app os)

--- app/browser/spellchecker.js
+++ app/browser/spellchecker.js
@@ -1,62 +1,42 @@
 /**
- * Enables spell-checking and the right-click context menu in text editors.
- * Electron (`webFrame.setSpellCheckProvider`) only underlines misspelled words;
- * we must manage the menu ourselves.
- *
- * Run this in the renderer process.
+ * This code comes from the electron-spell-check-provider README.md. Refer to it for further details.
+ * 
+ * I have remove most of the comments and extend it to detect the app locale. 
  */
-var remote = require('electron').remote;
-var webFrame = require('electron').webFrame;
-var SpellCheckProvider = require('electron-spell-check-provider');
-// `remote.require` since `Menu` is a main-process module.
-var buildEditorContextMenu = remote.require('electron-editor-context-menu');
-this.selection = {
-  isMisspelled: false,
-  spellingSuggestions:[]
-}
-
+const { remote, webFrame } = require('electron');
+const SpellCheckProvider = require('electron-spell-check-provider');
+const buildEditorContextMenu = remote.require('electron-editor-context-menu');
+const appLocale = remote.app.getLocale();
+let selection;
 
 function resetSelection() {
-  this.selection = {
+  selection = {
     isMisspelled: false,
     spellingSuggestions: []
   };
 }
-resetSelection();
- 
-// Reset the selection when clicking around, before the spell-checker runs and the context menu shows.
+
+function updateSelectionWithSuggestions(suggestions) {
+  {
+    if (window.getSelection().toString()) {
+      selection.isMisspelled = true;
+      selection.spellingSuggestions = suggestions.slice(0, 5);
+    }
+  }
+}
+
+spellCheckProviderCallback = new SpellCheckProvider(appLocale).on('misspelling', updateSelectionWithSuggestions);
+
 window.addEventListener('mousedown', resetSelection);
  
-// The spell-checker runs when the user clicks on text and before the 'contextmenu' event fires.
-// Thus, we may retrieve spell-checking suggestions to put in the menu just before it shows.
-webFrame.setSpellCheckProvider(
-  'en-UK',
-  // Not sure what this parameter (`autoCorrectWord`) does: https://github.com/atom/electron/issues/4371
-  // The documentation for `webFrame.setSpellCheckProvider` passes `true` so we do too.
-  true,
-  new SpellCheckProvider('en-UK').on('misspelling', function(suggestions) {
-    // Prime the context menu with spelling suggestions _if_ the user has selected text. Electron
-    // may sometimes re-run the spell-check provider for an outdated selection e.g. if the user
-    // right-clicks some misspelled text and then an image.
-    if (window.getSelection().toString()) {
-      self.selection.isMisspelled = true;
-      // Take the first three suggestions if any.
-      console.log('suggest', suggestions);
-      self.selection.spellingSuggestions = suggestions.slice(0, 3);
-    }
-  }));
- 
-window.addEventListener('contextmenu', function(e) {
-  // Only show the context menu in text editors.
-  if (!e.target.closest('textarea, input, [contenteditable="true"]')) return;
- 
-  this.console.log('building Editor context menu with suggestions ', this.selection.spellingSuggestions);
-  var menu = buildEditorContextMenu(this.selection);
- 
-  // The 'contextmenu' event is emitted after 'selectionchange' has fired but possibly before the
-  // visible selection has changed. Try to wait to show the menu until after that, otherwise the
-  // visible selection will update after the menu dismisses and look weird.
-  setTimeout(function() {
+webFrame.setSpellCheckProvider(appLocale, true, spellCheckProviderCallback);
+
+window.addEventListener('contextmenu', (e) => {
+  if (!e.target.closest('textarea, input, [contenteditable="true"]')) return; 
+  setTimeout(() => {
+    var menu = buildEditorContextMenu(selection);
     menu.popup(remote.getCurrentWindow());
   }, 30);
 });
+
+resetSelection();
