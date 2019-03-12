46be0d92b3b5b310f3a40e6340e146f6ed0e759c
Ismael <ismael.martinez@postcodelottery.co.uk>
2019-03-12 21:03:10 +0000

    increased the size of the screens to select and improving the quality of the thumbnail. Also added the page as a child page of the main page

--- app/browser/capture-selector.js
+++ app/browser/capture-selector.js
@@ -1,31 +1,35 @@
-const { desktopCapturer, remote } = require('electron');
+const { remote } = require('electron');
 const { BrowserWindow } = remote;
 
 function selectSource(callback) {
-  const selectorWindow = new BrowserWindow({
-    title: 'Share Your Screen',
-    width: 1024,
-    height: 768
-  });
+	const selectorWindow = new BrowserWindow({
+		title: 'Share Your Screen',
+		modal: true,
+		frame: false,
+		parent: remote.getCurrentWindow(),
+		autoHideMenuBar: true,
+		width: 1024,
+		height: 768
+	});
 
-  selectorWindow.webContents.once('did-finish-load', function () {
-    selectorWindow.emit('pickDesktopMedia', ['screen', 'window']);
-  });
+	selectorWindow.webContents.once('did-finish-load', function () {
+		selectorWindow.emit('pickDesktopMedia', ['screen', 'window']);
+	});
 
-  // Launch fullscreen with DevTools open, usage: npm run debug
-  // selectorWindow.webContents.openDevTools()
-  // mainWindow.maximize()
-  // require('devtron').install()
+	// Launch fullscreen with DevTools open, usage: npm run debug
+	// selectorWindow.webContents.openDevTools()
+	// mainWindow.maximize()
+	// require('devtron').install()
 
-  const url = require('url').format({
-    protocol: 'file',
-    slashes: true,
-    pathname: require('path').join(__dirname, 'capture-selector.html')
-  });
+	const url = require('url').format({
+		protocol: 'file',
+		slashes: true,
+		pathname: require('path').join(__dirname, 'capture-selector.html')
+	});
 
-  selectorWindow.on('close', () => callback());
-  selectorWindow.on('choseDesktopMedia', callback);
-  selectorWindow.loadURL(url);
+	selectorWindow.on('close', () => callback());
+	selectorWindow.on('choseDesktopMedia', callback);
+	selectorWindow.loadURL(url);
 }
 
-module.exports = { selectSource }
+module.exports = { selectSource };
