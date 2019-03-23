1083d5b2eae98c4fc1283d001d1085dca2dd1bb4
ismaelmartinez <ismaelmartinez@gmail.com>
2019-03-23 22:49:10 +0000

    Adding the final touches for #77.

--- app/config/index.js
+++ app/config/index.js
@@ -49,7 +49,7 @@ function argv(configPath) {
 			},
 			customCSSName: {
 				default: '',
-				describe: 'custom CSS name for the packaged available options. Currently those are: "compactDark", "compactLight", "tweaks", "condensedDark" and "condensedLight" ',
+				describe: 'custom CSS name for the packaged available css files. Currently those are: "compactDark", "compactLight", "tweaks", "condensedDark" and "condensedLight" ',
 				type: 'string'
 			},
 			customCSSLocation: {
