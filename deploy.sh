a8de71ff56fae5a27235d0a908013484110ac8e9
noizo <noizo@me.com>
2019-04-26 15:34:15 +0200

    init

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir /opt/outlook
 cp -f dist/outlook-for-linux\ 0.1.17.AppImage /opt/outlook/outlook.appimage
-ln -fs /opt/outlook/outlook.appimage /usr/local/bin/outlook
+ln -fs /opt/outlook/outlook.appimage /usr/local/bin/outloo
+
