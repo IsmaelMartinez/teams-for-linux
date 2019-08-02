593fd5c2057ff3f375194cb2c14daa13a2de27e9
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2019-08-02 20:04:03 +0100

    wip trying to fix screensharing

--- app/browser/index.js
+++ app/browser/index.js
@@ -15,7 +15,7 @@
 	require('./zoom')();
 
 	require('./desktopShare/chromeApi');
-	
+
 	const iconPath = path.join(__dirname, '../assets/icons/icon-96x96.png');
 
 	new ActivityManager(ipcRenderer, iconPath).start();
@@ -23,7 +23,7 @@
 	if (config.enableDesktopNotificationsHack) {
 		pageTitleNotifications(ipcRenderer);
 	}
-	
+
 	document.addEventListener(
 		'DOMContentLoaded',
 		() => {
@@ -34,9 +34,9 @@
 				if(injector) {
 					enableChromeVideoAudioMeetings(injector);
 					disablePromoteStuff(injector);
-					
+
 					injector.get('settingsService').settingsService.refreshSettings();
-					
+
 				}
 				// Future tests can be done in here...
 				// angular.element(document).injector().get('settingsService').appConfig.replyBoxFocusAfterNewMessage = true;
@@ -47,22 +47,38 @@
 
 	function enableChromeVideoAudioMeetings(injector) {
 		injector.get('callingSupportService').oneOnOneCallingEnabled = true;
-		injector.get('callingSupportService').isChromeMeetingSingleVideoEnabled = true;
-		injector.get('callingSupportService').isChromeVideoOneOnOneEnabled = true;
-		injector.get('callingSupportService').isChromeVideoMultipartyEnabled = true;
-		injector.get('settingsService').appConfig.enableCallingChromeOneOnOne = true;
-		injector.get('settingsService').appConfig.callingEnableChromeMeetingSingleVideo = true;
-		injector.get('settingsService').appConfig.callingEnableChromeMultipartyVideo = true;
+
+		injector.get('callingSupportService').isDesktopApp	 = true;
+		// injector.get('callingSupportService').isChromeMeetingSingleVideoEnabled = true;
+		// injector.get('callingSupportService').isChromeVideoOneOnOneEnabled = true;
+		// injector.get('callingSupportService').isChromeVideoMultipartyEnabled = true;
+		injector.get('settingsService').appConfig.angularDebugInfoEnabled = true;
+		// injector.get('settingsService').appConfig.enableCallingChromeOneOnOne = true;
+		// injector.get('settingsService').appConfig.callingEnableChromeMeetingSingleVideo = true;
+		// injector.get('settingsService').appConfig.callingEnableChromeMultipartyVideo = true;
+		// injector.get('settingsService').appConfig.callingEnabledLinux = true;
 		injector.get('settingsService').appConfig.enableChromeScreenSharing = true;
-		injector.get('settingsService').appConfig.enableAddToChatButtonForMeetings = true;
-		injector.get('settingsService').appConfig.enableSharingOnlyCallChrome = true;
-		injector.get('settingsService').appConfig.enableScreenSharingToolbar = true;
+		// injector.get('settingsService').appConfig.enableAddToChatButtonForMeetings = true;
+		// injector.get('settingsService').appConfig.enableSharingOnlyCallChrome = true;
+		// injector.get('settingsService').appConfig.enableScreenSharingToolbar = true;
 		injector.get('settingsService').appConfig.enableCallingScreenPreviewLabel = true;
-		injector.get('settingsService').appConfig.callingEnableChromeOneToOneVideo = true;
-		injector.get('settingsService').appConfig.enableMeetingStartedNotificationWeb = true;
+		// injector.get('settingsService').appConfig.callingEnableChromeOneToOneVideo = true;
+		// injector.get('settingsService').appConfig.enableMeetingStartedNotificationWeb = true;
 		injector.get('settingsService').appConfig.enableMicOSUnmuteOnUnmute = true;
 		injector.get('settingsService').appConfig.enableModeratorsSupport = true;
 		injector.get('settingsService').appConfig.enableRecordPPTSharing = true;
+		injector.get('settingsService').appConfig.enable3x3VideoLayout = true;
+		injector.get('settingsService').appConfig.enableCallTranscript = true;
+		injector.get('settingsService').appConfig.enableCallTransferredScreen = true;
+		injector.get('settingsService').appConfig.enableCameraSharing = true;
+		injector.get('settingsService').appConfig.enableEdgeScreenSharing = true;
+		injector.get('settingsService').appConfig.enableSeeMyScreenshare = true;
+		injector.get('settingsService').appConfig.enableSmartReplies = true;
+		injector.get('settingsService').appConfig.enableSms = true;
+		injector.get('settingsService').appConfig.enableTestCallForAll = true;
+		injector.get('settingsService').appConfig.enableUnreadMessagesButton = true;
+		injector.get('settingsService').appConfig.enableVideoBackground = true;
+		injector.get('settingsService').appConfig.disableCallingOnlineCheck = false;
 	}
 
 	function disablePromoteStuff(injector) {
@@ -72,3 +88,4 @@
 		injector.get('settingsService').appConfig.enableMobileDownloadMailDialog = false;
 	}
 }());
+
