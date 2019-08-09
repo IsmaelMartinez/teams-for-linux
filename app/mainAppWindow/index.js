b330962af6f1653270d795ccf83d87373d1b5148
IsmaelMartinez <ISMAELMARTINEZ@GMAIL.COM>
2019-08-09 10:50:06 +0100

    Feature 107 adding extra logs and checks (#213)
    * Adding extra logs and the flushStorageData to see if fixes, or help, with #107
    
    * Adding teh clear appcache storage

--- app/mainAppWindow/index.js
+++ app/mainAppWindow/index.js
@@ -16,6 +16,7 @@ let window = null;
 exports.onAppReady = function onAppReady() {
 	window = createWindow();
 	window.webContents.session.clearCache(()=> console.log("session cache cleared"));
+	window.webContents.session.clearStorageData({ storages: ['appcache']} ,()=> console.log("appcache cleared"));
 	new Menus(window, config, iconPath);
 
 	window.on('page-title-updated', (event, title) => {
