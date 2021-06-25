890845c5f08725109965f92c259c0f534a0a5188
Jijo Joseph <jijojosephk@hotmail.com>
2021-06-25 08:34:09 +0530

    #432 Code refactoring phase 2

--- app/streamSelector/browser.js
+++ app/streamSelector/browser.js
@@ -13,98 +13,100 @@ function createSourceSelector() {
 function createPreviewScreen(screens) {
 	let windowsIndex = 0;
 	const sscontainer = document.getElementById('screen-size');
-	createEventHandlers();
+	createEventHandlers(screens, sscontainer);
 	desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async (sources) => {
 		const rowElement = document.querySelector('.container-fluid .row');
 		for (const source of sources) {
-			await generatePreview(source, rowElement);
+			windowsIndex = await createPreview(source, windowsIndex, rowElement, screens, sscontainer);
 		}
 	});
+}
 
-	async function generatePreview(source, rowElement) {
-		let columnElement = document.createElement('div');
-		columnElement.className = `col-5 ${source.id.startsWith('screen:') ? 'screen' : 'window'}`;
-		// Video container
-		let videoContainerElement = document.createElement('div');
-		videoContainerElement.className = 'video-container';
-		// Video
-		let videoElement = document.createElement('video');
-		videoElement.setAttribute('data-id', source.id);
-		videoElement.title = source.name;
-		videoContainerElement.appendChild(videoElement);
-		// Label
-		let labelElement = document.createElement('div');
-		labelElement.className = 'label-container';
-		labelElement.appendChild(document.createTextNode(source.id.startsWith('screen:') ? source.name : `Window ${++windowsIndex}`));
-		columnElement.appendChild(videoContainerElement);
-		columnElement.appendChild(labelElement);
-		rowElement.appendChild(columnElement);
-		const stream = await navigator.mediaDevices.getUserMedia({
-			audio: false,
-			video: {
-				mandatory: {
-					chromeMediaSource: 'desktop',
-					chromeMediaSourceId: source.id,
-					minWidth: 192,
-					maxWidth: 192,
-					minHeight: 108,
-					maxHeight: 108
-				}
+async function createPreview(source, windowsIndex, rowElement, screens, sscontainer) {
+	let columnElement = document.createElement('div');
+	columnElement.className = `col-5 ${source.id.startsWith('screen:') ? 'screen' : 'window'}`;
+	// Video container
+	let videoContainerElement = document.createElement('div');
+	videoContainerElement.className = 'video-container';
+	// Video
+	let videoElement = document.createElement('video');
+	videoElement.setAttribute('data-id', source.id);
+	videoElement.title = source.name;
+	videoContainerElement.appendChild(videoElement);
+	// Label
+	let labelElement = document.createElement('div');
+	labelElement.className = 'label-container';
+	labelElement.appendChild(document.createTextNode(source.id.startsWith('screen:') ? source.name : `Window ${++windowsIndex}`));
+	columnElement.appendChild(videoContainerElement);
+	columnElement.appendChild(labelElement);
+	rowElement.appendChild(columnElement);
+	const stream = await navigator.mediaDevices.getUserMedia({
+		audio: false,
+		video: {
+			mandatory: {
+				chromeMediaSource: 'desktop',
+				chromeMediaSourceId: source.id,
+				minWidth: 192,
+				maxWidth: 192,
+				minHeight: 108,
+				maxHeight: 108
 			}
-		});
-		playPreview();
-
-		function playPreview() {
-			videoElement.srcObject = stream;
-			videoElement.onclick = () => {
-				closePreviews();
-				ipcRenderer.send('selected-source', {
-					id: source.id,
-					screen: screens[sscontainer.value]
-				});
-			};
-			videoElement.onloadedmetadata = () => videoElement.play();
 		}
-	}
+	});
+	playPreview(videoElement, stream, source, screens, sscontainer);
+	return windowsIndex;
+}
 
-	function createEventHandlers() {
-		createQualitySelector();
-		document.querySelector('#btn-screens').addEventListener('click', toggleSources);
-		document.querySelector('#btn-windows').addEventListener('click', toggleSources);
-		document.querySelector('#btn-close').addEventListener('click', () => {
-			closePreviews();
-			ipcRenderer.send('close-view');
+function playPreview(videoElement, stream, source, screens, sscontainer) {
+	videoElement.srcObject = stream;
+	videoElement.onclick = () => {
+		closePreviews();
+		ipcRenderer.send('selected-source', {
+			id: source.id,
+			screen: screens[sscontainer.value]
 		});
-	}
+	};
+	videoElement.onloadedmetadata = () => videoElement.play();
+}
 
-	function closePreviews() {
-		const vidElements = document.getElementsByTagName('video');
-		for (const vidElement of vidElements) {
-			vidElement.pause();
-			vidElement.srcObject.getVideoTracks()[0].stop();
-		}
-	}
+function createEventHandlers(screens, sscontainer) {
+	createQualitySelector(screens, sscontainer);
+	document.querySelector('#btn-screens').addEventListener('click', toggleSources);
+	document.querySelector('#btn-windows').addEventListener('click', toggleSources);
+	document.querySelector('#btn-close').addEventListener('click', () => {
+		closePreviews();
+		ipcRenderer.send('close-view');
+	});
+}
 
-	function toggleSources(e) {
-		document.querySelectorAll('button').forEach(b => {
-			b.classList.toggle('btn-primary');
-			b.classList.toggle('btn-secondary');
-		});
-		document.querySelector('.container-fluid').setAttribute('data-view', e.target.getAttribute('data-view'));
-	}
-
-	function createQualitySelector() {
-		screens.forEach((s, i) => {
-			const opt = document.createElement('option');
-			opt.appendChild(document.createTextNode(s.name));
-			opt.value = i;
-			sscontainer.appendChild(opt);
-		});
-		let defaultSelection = screens.findIndex(s => {
-			return s.default;
-		});
-
-		defaultSelection = defaultSelection > -1 ? defaultSelection : screens.length - 1;
-		sscontainer.selectedIndex = defaultSelection;
+function closePreviews() {
+	const vidElements = document.getElementsByTagName('video');
+	for (const vidElement of vidElements) {
+		vidElement.pause();
+		vidElement.srcObject.getVideoTracks()[0].stop();
 	}
 }
+
+function toggleSources(e) {
+	document.querySelectorAll('button').forEach(b => {
+		b.classList.toggle('btn-primary');
+		b.classList.toggle('btn-secondary');
+	});
+	document.querySelector('.container-fluid').setAttribute('data-view', e.target.getAttribute('data-view'));
+}
+
+function createQualitySelector(screens, sscontainer) {
+	screens.forEach((s, i) => {
+		const opt = document.createElement('option');
+		opt.appendChild(document.createTextNode(s.name));
+		opt.value = i;
+		sscontainer.appendChild(opt);
+	});
+	let defaultSelection = screens.findIndex(s => {
+		return s.default;
+	});
+
+	defaultSelection = defaultSelection > -1 ? defaultSelection : screens.length - 1;
+	sscontainer.selectedIndex = defaultSelection;
+}
+
