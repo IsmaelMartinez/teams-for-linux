5ef2b3b356cff37975b9bfb101ee21a935b519da
Jijo Joseph <jijojosephk@hotmail.com>
2021-06-25 09:17:16 +0530

    #432 Code refactoring phase 5

--- app/browser/desktopShare/chromeApi.js
+++ app/browser/desktopShare/chromeApi.js
@@ -2,36 +2,42 @@ const { ipcRenderer } = require('electron');
 // In order to have this functionality working, contextIsolation should be disabled.
 // In new versions of electron, contextIsolation is set to true by default.
 // We should explicitly set it to false when creating BrowserWindow
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', overrideGetDisplayMedia);
+
+function overrideGetDisplayMedia() {
 	MediaDevices.prototype.getDisplayMedia = () => {
 		return new Promise((resolve, reject) => {
 			// Request main process to allow access to screen sharing
 			ipcRenderer.once('select-source', (event, source) => {
-				if (source) {
-					navigator.mediaDevices.getUserMedia({
-						audio: false,
-						video: {
-							mandatory: {
-								chromeMediaSource: 'desktop',
-								chromeMediaSourceId: source.id,
-								minWidth: source.screen.width,
-								maxWidth: source.screen.width,
-								minHeight: source.screen.height,
-								maxHeight: source.screen.height
-							}
-						}
-					}).then(stream => {
-						resolve(stream);
-					}).catch(e => {
-						console.log(e.message);
-						reject(e.message);
-					});
-				} else {
-					reject('Access denied');
-				}
+				startStreaming({ source, resolve, reject });
 			});
-
 			ipcRenderer.send('select-source');
 		});
 	};
-});
+}
+
+function startStreaming(properties) {
+	if (properties.source) {
+		navigator.mediaDevices.getUserMedia({
+			audio: false,
+			video: {
+				mandatory: {
+					chromeMediaSource: 'desktop',
+					chromeMediaSourceId: properties.source.id,
+					minWidth: properties.source.screen.width,
+					maxWidth: properties.source.screen.width,
+					minHeight: properties.source.screen.height,
+					maxHeight: properties.source.screen.height
+				}
+			}
+		}).then(stream => {
+			properties.resolve(stream);
+		}).catch(e => {
+			console.log(e.message);
+			properties.reject(e.message);
+		});
+	} else {
+		properties.reject('Access denied');
+	}
+}
+
