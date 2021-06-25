1a1abbab154c86b8bd7bf2e8c79044e0b80ccdbd
Jijo Joseph <jijojosephk@hotmail.com>
2021-06-25 08:12:06 +0530

    #432 Code refactoring phase 1

--- app/streamSelector/browser.js
+++ app/streamSelector/browser.js
@@ -1,8 +1,99 @@
 const { desktopCapturer, ipcRenderer } = require('electron');
-window.addEventListener('DOMContentLoaded', () => {
-	ipcRenderer.once('get-screensizes-response', (event, screens) => {
-		let windowsIndex = 0;
-		const sscontainer = document.getElementById('screen-size');
+window.addEventListener('DOMContentLoaded', createSourceSelector());
+
+function createSourceSelector() {
+	return () => {
+		ipcRenderer.once('get-screensizes-response', (event, screens) => {
+			createPreviewScreen(screens);
+		});
+		ipcRenderer.send('get-screensizes-request');
+	};
+}
+
+function createPreviewScreen(screens) {
+	let windowsIndex = 0;
+	const sscontainer = document.getElementById('screen-size');
+	createEventHandlers();
+	desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async (sources) => {
+		const rowElement = document.querySelector('.container-fluid .row');
+		for (const source of sources) {
+			await generatePreview(source, rowElement);
+		}
+	});
+
+	async function generatePreview(source, rowElement) {
+		let columnElement = document.createElement('div');
+		columnElement.className = `col-5 ${source.id.startsWith('screen:') ? 'screen' : 'window'}`;
+		// Video container
+		let videoContainerElement = document.createElement('div');
+		videoContainerElement.className = 'video-container';
+		// Video
+		let videoElement = document.createElement('video');
+		videoElement.setAttribute('data-id', source.id);
+		videoElement.title = source.name;
+		videoContainerElement.appendChild(videoElement);
+		// Label
+		let labelElement = document.createElement('div');
+		labelElement.className = 'label-container';
+		labelElement.appendChild(document.createTextNode(source.id.startsWith('screen:') ? source.name : `Window ${++windowsIndex}`));
+		columnElement.appendChild(videoContainerElement);
+		columnElement.appendChild(labelElement);
+		rowElement.appendChild(columnElement);
+		const stream = await navigator.mediaDevices.getUserMedia({
+			audio: false,
+			video: {
+				mandatory: {
+					chromeMediaSource: 'desktop',
+					chromeMediaSourceId: source.id,
+					minWidth: 192,
+					maxWidth: 192,
+					minHeight: 108,
+					maxHeight: 108
+				}
+			}
+		});
+		playPreview();
+
+		function playPreview() {
+			videoElement.srcObject = stream;
+			videoElement.onclick = () => {
+				closePreviews();
+				ipcRenderer.send('selected-source', {
+					id: source.id,
+					screen: screens[sscontainer.value]
+				});
+			};
+			videoElement.onloadedmetadata = () => videoElement.play();
+		}
+	}
+
+	function createEventHandlers() {
+		createQualitySelector();
+		document.querySelector('#btn-screens').addEventListener('click', toggleSources);
+		document.querySelector('#btn-windows').addEventListener('click', toggleSources);
+		document.querySelector('#btn-close').addEventListener('click', () => {
+			closePreviews();
+			ipcRenderer.send('close-view');
+		});
+	}
+
+	function closePreviews() {
+		const vidElements = document.getElementsByTagName('video');
+		for (const vidElement of vidElements) {
+			vidElement.pause();
+			vidElement.srcObject.getVideoTracks()[0].stop();
+		}
+	}
+
+	function toggleSources(e) {
+		document.querySelectorAll('button').forEach(b => {
+			b.classList.toggle('btn-primary');
+			b.classList.toggle('btn-secondary');
+		});
+		document.querySelector('.container-fluid').setAttribute('data-view', e.target.getAttribute('data-view'));
+	}
+
+	function createQualitySelector() {
 		screens.forEach((s, i) => {
 			const opt = document.createElement('option');
 			opt.appendChild(document.createTextNode(s.name));
@@ -15,73 +106,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		defaultSelection = defaultSelection > -1 ? defaultSelection : screens.length - 1;
 		sscontainer.selectedIndex = defaultSelection;
-
-		const _tabClick = (e) => {
-			document.querySelectorAll('button').forEach(b => {
-				b.classList.toggle('btn-primary');
-				b.classList.toggle('btn-secondary');
-			});
-			document.querySelector('.container-fluid').setAttribute('data-view', e.target.getAttribute('data-view'));
-		};
-
-		const _closePreviews = () => {
-			const vidElements = document.getElementsByTagName('video');
-			for (const vidElement of vidElements) {
-				vidElement.pause();
-				vidElement.srcObject.getVideoTracks()[0].stop();
-			}
-		};
-
-		document.querySelector('#btn-screens').addEventListener('click', _tabClick);
-		document.querySelector('#btn-windows').addEventListener('click', _tabClick);
-		document.querySelector('#btn-close').addEventListener('click', () => {
-			_closePreviews();
-			ipcRenderer.send('close-view');
-		});
-		desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
-			const rowElement = document.querySelector('.container-fluid .row');
-			for (const source of sources) {
-				let columnElement = document.createElement('div');
-				columnElement.className = `col-5 ${source.id.startsWith('screen:') ? 'screen' : 'window'}`;
-				// Video container
-				let videoContainerElement = document.createElement('div');
-				videoContainerElement.className = 'video-container';
-				// Video
-				let videoElement = document.createElement('video');
-				videoElement.setAttribute('data-id', source.id);
-				videoElement.title = source.name;
-				videoContainerElement.appendChild(videoElement);
-				// Label
-				let labelElement = document.createElement('div');
-				labelElement.className = 'label-container';
-				labelElement.appendChild(document.createTextNode(source.id.startsWith('screen:') ? source.name : `Window ${++windowsIndex}`));
-				columnElement.appendChild(videoContainerElement);
-				columnElement.appendChild(labelElement);
-				rowElement.appendChild(columnElement);
-				const stream = await navigator.mediaDevices.getUserMedia({
-					audio: false,
-					video: {
-						mandatory: {
-							chromeMediaSource: 'desktop',
-							chromeMediaSourceId: source.id,
-							minWidth: 192,
-							maxWidth: 192,
-							minHeight: 108,
-							maxHeight: 108
-						}
-					}
-				});
-				videoElement.srcObject = stream;
-				videoElement.onclick = () => {
-					_closePreviews();
-					ipcRenderer.send('selected-source', {
-						id: source.id,
-						screen: screens[sscontainer.value]
-					});
-				};
-				videoElement.onloadedmetadata = () => videoElement.play();
-			}
-		});
-	});
-	ipcRenderer.send('get-screensizes-request');
-});
+	}
+}
