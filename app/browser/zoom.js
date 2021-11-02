d0688042129b2c76d5fde7a2688298b92d1b039b
Jijo Joseph <jijojosephk@hotmail.com>
2021-11-02 14:28:21 +0530

    Issue 447 (#501)
    * Fix: #447
    
    * Fix: #447
    
    * Fix: #447

--- app/browser/zoom.js
+++ app/browser/zoom.js
@@ -1,6 +1,13 @@
-const {webFrame} = require('electron');
+const { webFrame, ipcRenderer } = require('electron');
 
-exports = module.exports = () => {
+const zoomLevels = {
+	'+': 1,
+	'-': -1,
+	'0': 0
+};
+
+exports = module.exports = (config) => {
+	restoreZoomLevel(config);
 	document.addEventListener('keydown', (event) => {
 		const keyName = event.key;
 
@@ -10,11 +17,29 @@ exports = module.exports = () => {
 		}
 
 		if (event.ctrlKey) {
-			if (keyName === '+') {
-				webFrame.setZoomLevel(webFrame.getZoomLevel() + 1);
-			} else if (keyName === '-') {
-				webFrame.setZoomLevel(webFrame.getZoomLevel() - 1);
-			}
+			setNextZoomLevel(keyName, config);
 		}
 	}, false);
 };
+
+function restoreZoomLevel(config) {
+	ipcRenderer.invoke('getZoomLevel', config.partition).then(zoomLevel => {
+		webFrame.setZoomLevel(zoomLevel);
+	});
+}
+
+function setNextZoomLevel(keyName, config) {
+	const zoomFactor = zoomLevels[keyName];
+	var zoomLevel = webFrame.getZoomLevel();
+	console.log(`Current zoom level: ${zoomLevel}`);
+	if (typeof (zoomFactor) !== 'number') {
+		return;
+	}
+
+	zoomLevel = zoomFactor == 0 ? 0 : zoomLevel + zoomFactor;
+	webFrame.setZoomLevel(zoomLevel);
+	ipcRenderer.invoke('saveZoomLevel', {
+		partition: config.partition,
+		zoomLevel: webFrame.getZoomLevel()
+	});
+}
