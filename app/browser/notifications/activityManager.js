8e18f054479db351ec0a14e47c2a469e01f9d53b
Jijo Joseph <jijojosephk@hotmail.com>
2022-10-01 16:07:58 +0530

    Activity manager re-design

--- app/browser/notifications/activityManager.js
+++ app/browser/notifications/activityManager.js
@@ -1,15 +1,14 @@
-/* global angular, teamspace */
 const TrayIconRenderer = require('./trayIconRenderer');
+const activityHub = require('./activityHub');
 
 class ActivityManager {
-
 	constructor(ipc, baseIconPath) {
 		this.ipc = ipc;
 		this.subscribed = false;
 		this.iconRenderer = new TrayIconRenderer(baseIconPath);
 	}
 
-	handleNewActivityCountUpdate(count) {
+	updateActivityCount(count) {
 		this.iconRenderer.render(count).then(icon => {
 			this.ipc.send('tray-update', {
 				icon: icon,
@@ -18,50 +17,9 @@ class ActivityManager {
 		});
 	}
 
-	subscribeToNewActivitiyCountUpdate(callback) {
-		if (this.subscribed) {
-			return;
-		}
-	
-		if (typeof angular === 'undefined') {
-			return;
-		}
-	
-		const controller = angular.element(document.documentElement).controller();
-		
-		if (!controller) {
-			return;
-		}
-
-		const onChange = () => {
-			const count = 
-				controller.bellNotificationsService.getNewActivitiesCount() +
-				controller.chatListService.getUnreadCountFromChatList();
-
-			callback(count);
-		};
-	
-		controller.eventingService.$on(
-			controller.$scope, 
-			controller.constants.events.notifications.bellCountUpdated, 
-			onChange);
-	
-		controller.chatListService.safeSubscribe(
-			controller.$scope, 
-			onChange, 
-			teamspace.services.ChatListServiceEvents.EventType_UnreadCount);
-	
-		this.subscribed = true;
-
-		onChange();
-	}
-
 	start() {
-		this.ipc.on('page-title', () => {
-			this.subscribeToNewActivitiyCountUpdate(count => {
-				this.handleNewActivityCountUpdate(count);
-			});
-		});
+		activityHub.on('activities-count-updated', (data) => this.updateActivityCount(data.count));
+		activityHub.start();
 	}
 }
 
