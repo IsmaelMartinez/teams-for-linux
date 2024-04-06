44ab1963c2ae4de1a7848aed25ca82377b772a97
Jijo Joseph <jijojosephk@hotmail.com>
2024-04-07 00:23:31 +0530

    Fixed notification badge count does not clear when none pending

--- app/browser/tools/mutationTitle.js
+++ app/browser/tools/mutationTitle.js
@@ -15,12 +15,10 @@ class MutationObserverTitle {
 				console.log(window.document.title);
 				const regex = /^\((\d+)\)/;
 				const match = window.document.title.match(regex);
-				if (match) {
-					const number = match[1];
-					console.log(number);
-					const event = new CustomEvent('unread-count', { detail: { number: number } });
-					window.dispatchEvent(event);
-				}
+				const number = match ? match[1] : 0;
+				console.log(number);
+				const event = new CustomEvent('unread-count', { detail: { number: number } });
+				window.dispatchEvent(event);
 			}
 		);
 		observer.observe(window.document.querySelector('title'),{ childList: true });
