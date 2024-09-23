56701652d01df9f74718bc5cb314fd56c7f63f0d
IsmaelMartinez <ISMAELMARTINEZ@GMAIL.COM>
2024-09-23 21:06:38 +0100

    Feature/move notification number bottom right (#1415)
    * moving the notification number to be in the tux area. This seems to be the most sensible area to put it
    
    * adding release notes

--- app/browser/tools/trayIconRenderer.js
+++ app/browser/tools/trayIconRenderer.js
@@ -40,16 +40,16 @@ class TrayIconRenderer {
 		if (newActivityCount > 0) {
 			ctx.fillStyle = 'red';
 			ctx.beginPath();
-			ctx.ellipse(105, 35, 35, 35, 35, 0, 2 * Math.PI);
+			ctx.ellipse(100, 90, 40, 40, 40, 0, 2 * Math.PI);
 			ctx.fill();
 			ctx.textAlign = 'center';
 			ctx.fillStyle = 'white';
 
 			ctx.font = 'bold 70px "Segoe UI","Helvetica Neue",Helvetica,Arial,sans-serif';
 			if (newActivityCount > 9) {
-				ctx.fillText('9+', 105, 60);
+				ctx.fillText('+', 100, 110);
 			} else {
-				ctx.fillText(newActivityCount.toString(), 105, 60);
+				ctx.fillText(newActivityCount.toString(), 100, 110);
 			}
 		}
 		const resizedCanvas = this._getResizeCanvasWithOriginalIconSize(canvas);
