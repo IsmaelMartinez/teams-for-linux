2800067328ee6c10b068ba8fdeab10d57a9ea149
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2024-10-13 21:54:40 +0100

    fixes on the activityManager and updating dependencies

--- app/browser/notifications/activityManager.js
+++ app/browser/notifications/activityManager.js
@@ -21,34 +21,34 @@ class ActivityManager {
 		self.ipcRenderer.invoke('get-system-idle-state').then((state) => {
 			let timeOut;
 			if (this.config.awayOnSystemIdle) {
-				timeOut = setStatusAwayWhenScreenLocked(state);
+				timeOut = this.setStatusAwayWhenScreenLocked(state);
 			} else {
-				timeOut = keepStatusAvailableWhenScreenLocked(state);
+				timeOut = this.keepStatusAvailableWhenScreenLocked(state);
 			}
 			setTimeout(() => self.watchSystemIdleState(), timeOut);
 		});
 	}
-}
+    
+	setStatusAwayWhenScreenLocked(state) {
+		activityHub.setMachineState(state.system === 'active' ? 1 : 2);
+		const timeOut = (state.system === 'active' ? this.config.appIdleTimeoutCheckInterval : this.config.appActiveCheckInterval) * 1000;
 
-function setStatusAwayWhenScreenLocked(state) {
-	activityHub.setMachineState(state.system === 'active' ? 1 : 2);
-	const timeOut = (state.system === 'active' ? self.config.appIdleTimeoutCheckInterval : self.config.appActiveCheckInterval) * 1000;
-
-	if (state.system === 'active' && state.userIdle === 1) {
-		activityHub.setUserStatus(1);
-	} else if (state.system !== 'active' && state.userCurrent === 1) {
-		activityHub.setUserStatus(3);
+		if (state.system === 'active' && state.userIdle === 1) {
+			activityHub.setUserStatus(1);
+		} else if (state.system !== 'active' && state.userCurrent === 1) {
+			activityHub.setUserStatus(3);
+		}
+		return timeOut;
 	}
-	return timeOut;
-}
 
-function keepStatusAvailableWhenScreenLocked(state) {
-	if ((state.system === 'active') || (state.system === 'locked')) {
-		activityHub.setMachineState(1);
-		return self.config.appIdleTimeoutCheckInterval * 1000;
+	keepStatusAvailableWhenScreenLocked(state) {
+		if ((state.system === 'active') || (state.system === 'locked')) {
+			activityHub.setMachineState(1);
+			return this.config.appIdleTimeoutCheckInterval * 1000;
+		}
+		activityHub.setMachineState(2);
+		return this.config.appActiveCheckInterval * 1000;
 	}
-	activityHub.setMachineState(2);
-	return self.config.appActiveCheckInterval * 1000;
 }
 
 function setActivityHandlers(self) {
